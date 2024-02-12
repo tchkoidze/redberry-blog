1b93a7eec4b57eed96c398122270716c7daf5635
tchkoidze <ilia.chkoidze12@gmail.com>
2024-02-12 17:20:51 +0400

    fix: errors at add blog page

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -239,12 +239,12 @@ const Blog = () => {
                 rx="22"
                 transform="rotate(180 44 44)"
                 className={`${
-                  startIndex + 4 < (filteredBlogs?.length ?? 0)
+                  startIndex + 4 <= (filteredBlogs?.length ?? 0)
                     ? "hover:fill-[#512BE7] active:fill-[#4721DD]"
                     : "hover:fill-[#D9D8E0] active:fill-[#CFCED6]"
                 }`}
                 fill={
-                  startIndex + 4 < (filteredBlogs?.length ?? 0)
+                  startIndex + 4 <= (filteredBlogs?.length ?? 0)
                     ? "#5D37F3"
                     : "#E4E3EB"
                 }
@@ -252,7 +252,7 @@ const Blog = () => {
               <path
                 d="M26 21C25.4477 21 25 21.4477 25 22C25 22.5523 25.4477 23 26 23L26 21ZM26.8071 22.7071C27.1976 22.3166 27.1976 21.6834 26.8071 21.2929L20.4431 14.9289C20.0526 14.5384 19.4195 14.5384 19.0289 14.9289C18.6384 15.3195 18.6384 15.9526 19.0289 16.3431L24.6858 22L19.0289 27.6569C18.6384 28.0474 18.6384 28.6805 19.0289 29.0711C19.4195 29.4616 20.0526 29.4616 20.4431 29.0711L26.8071 22.7071ZM26 23L26.1 23L26.1 21L26 21L26 23Z"
                 fill={
-                  startIndex + 4 < (filteredBlogs?.length ?? 0)
+                  startIndex + 4 <= (filteredBlogs?.length ?? 0)
                     ? "white"
                     : "#1A1A1F"
                 }
